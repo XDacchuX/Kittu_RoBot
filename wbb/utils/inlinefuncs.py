27bca9ad8360a583a56dc5b3e7342f4c1ba0e953
Darshan <92586012+XDacchuX@users.noreply.github.com>
2022-02-08 15:19:22 +0530

    Update inlinefuncs.py

--- wbb/utils/inlinefuncs.py
+++ wbb/utils/inlinefuncs.py
@@ -93,7 +93,7 @@ async def inline_help_func(__HELP__):
             title="Github Repo",
             description="Get Github Respository Of Bot.",
             input_message_content=InputTextMessageContent(
-                "https://github.com/thehamkercat/WilliamButcherBot"
+                "https://github.com/XDacchuX/RazeRoBot"
             ),
             thumb_url="https://hamker.me/gjc9fo3.png",
         ),
@@ -114,7 +114,7 @@ async def alive_function(answers):
     )
 
     msg = f"""
-**[William✨](https://github.com/thehamkercat/WilliamButcherBot):**
+**[𝗥𝗮𝘇𝗲 𝗥𝗼𝗯𝗼𝘁](https://github.com/XDacchuX/RazeRoBot):**
 **MainBot:** `{bot_state}`
 **UserBot:** `{ubot_state}`
 **Python:** `{pyver.split()[0]}`
